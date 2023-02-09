32350be7706e6fbfd44876275ebfb6c7fbaecb53
acfuns <1haolong.sun@gmail.com>
2023-02-09 13:59:03 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+useHead({
+  title: '母婴产品商城',
+  link: [
+    {
+      rel: 'icon', type: 'image/png', href: '/nuxt.png',
+    },
+  ],
+})
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style>
+html, body , #__nuxt{
+  height: 200vh;
+  margin: 0;
+  padding: 0;
+}
+
+html.dark {
+  background: #222;
+  color: white;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="sticky top-0 z-30  py-3 dark:text-[#BBBBBB] text-gray-400 text-xl backdrop-blur-md shadow shadow-gray-200 dark:shadow-gray-800">
+    <div class="mx-auto sm:px-6 lg:px-8 px-4 max-w-7xl relative">
+      <div class="flex justify-between items-center">
+        <div class="flex items-center">
+          <img class="w-14 h-14 inline-block" src="/nuxt.png">
+          <span class="ml-1 text-xl">母婴产品商城</span>
+        </div>
+        <div class="grid grid-flow-col gap-4 items-center">
+          <div class="rounded cursor-pointer dark:hover:text-gray-200 hover:duration-0.4s hover:text-gray-800">
+            个人中心
+          </div>
+          <div class="rounded cursor-pointer dark:hover:text-gray-200 hover:duration-0.4s hover:text-gray-800">
+            购物车
+          </div>
+          <div class="rounded cursor-pointer dark:hover:text-gray-200 hover:duration-0.4s hover:text-gray-800">
+            注册登录
+          </div>
+          <DarkToggle />
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
